5b9735335791fd6bd02c969cf9e8403194ca7d7d
Andy Hamilton <andyhmltn@gmail.com>
2012-02-01 16:10:19 +0000

    The controllers folder wasn't being tracked because there was no files inside of it. I added a basic index controller with instructions

---
+++ application/controllers/indexController.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * THIS IS A BLANK CONTROLLER. TO GENERATE
+ * A NEW CONTROLLER OPEN THE COMMAND LINE TOOL
+ * AND TYPE THE FOLLOWING:
+ *
+ * 		`generate controller controllername firstaction secondaction etcaction`
+ *
+ */
+
+ <?php
+
+class IndexController extends Controller {
+
+	public $except = array(/** What not to run the _before action before. **/);
+	
+	function _before() {
+		/** Before action **/
+	}
+
+	function index() {
+		#index
+	}
+
+}
+
+
+?>
